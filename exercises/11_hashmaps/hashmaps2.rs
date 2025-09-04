bc3ce5f0ca76d10a3c6aa869a68f45e8059ea280
Oleksii Khilkevych <oleksiy@noroutine.me>
2025-09-04 20:11:13 +0200

    11

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -28,10 +28,14 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         Fruit::Pineapple,
     ];
 
+    let mut fib = (2, 3);
     for fruit in fruit_kinds {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        let next = fib.0 + fib.1;
+        basket.entry(fruit).or_insert( next);
+        fib = (fib.1, next)
     }
 }
 
